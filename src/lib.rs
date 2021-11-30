9a65fd5acaa9225e452b0e4a8e46d5c371f311bd
Jacques Marneweck <jacques@siberia.co.za>
2021-11-30 22:38:15 +0200

    Add license header

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
 #![no_std]
 
 //! idt8a3xxxx: A crate for drivers for the Renesas 8A3XXXX series
